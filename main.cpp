4298b0782a21664f9c56a2625e61ff914a017524
vvolf02 <160361495+vvolf02@users.noreply.github.com>
2024-03-27 17:18:21 -0500

    commit 4
    adaptación de las funciones de rotación y creación de matrices

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 using namespace std;
-void rotar90(int matriz[5][5]);
-void rotar180(int matriz[5][5]);
-void rotar270(int matriz[5][5]);
+void rotar90(int matriz[n][n]);
+void rotar180(int matriz[n][n]);
+void rotar270(int matriz[n][n]);
 void punto14();
-int** crearmatriz(int nums[], int n);
+int** crearmatriz(int n);
 int main()
 {
     int n;
@@ -29,10 +29,10 @@ int main()
     }
 }
 
-int** crearmatriz(int nums[], int n)
+int** crearmatriz(int n)
 {
     int num,num1,val=1;
-    num=nums[n];
+    num=n
     num1=num/2;
     int **matriz=new int*[num];
     for(int i=0; i<num; i++)
@@ -58,41 +58,41 @@ int** crearmatriz(int nums[], int n)
 }
 
 //funciones traidas de la practica de laboratorio 2
-void rotar90(int matriz[5][5])
+void rotar90(int matriz[n][n])
 {
-    int matrot[5][5];
-    for (int i=0; i<5; i++)
+    int matrot[n][n];
+    for (int i=0; i<n; i++)
     {
-        for (int j=0; j<5; j++)
+        for (int j=0; j<n; j++)
         {
-            matrot[j][5-1-i]=matriz[i][j];
+            matrot[j][n-1-i]=matriz[i][j];
         }
     }
     cout<<"Matriz rotada 90 grados:"<<endl;
 }
 
-void rotar180(int matriz[5][5])
+void rotar180(int matriz[n][n])
 {
-    int matrot[5][5];
-    for (int i=0; i<5; i++)
+    int matrot[n][n];
+    for (int i=0; i<n; i++)
     {
-        for (int j=0; j<5; j++)
+        for (int j=0; j<n; j++)
         {
-            matrot[5-1-i][5-1-j]=matriz[i][j];
+            matrot[n-1-i][n-1-j]=matriz[i][j];
         }
     }
 
     cout<<"Matriz rotada 180 grados:"<<endl;
 }
 
-void rotar270(int matriz[5][5])
+void rotar270(int matriz[n][n])
 {
-    int matrot[5][5];
-    for (int i=0; i<5; i++)
+    int matrot[n][n];
+    for (int i=0; i<n; i++)
     {
-        for (int j=0; j<5; j++)
+        for (int j=0; j<n; j++)
         {
-            matrot[5-1-j][i]=matriz[i][j];
+            matrot[n-1-j][i]=matriz[i][j];
         }
     }
 
@@ -101,7 +101,7 @@ void rotar270(int matriz[5][5])
 
 void punto14()
 {
-    int matriz[5][5];
+    int matriz[n][n];
     int cont=1;
     for (int i=0; i<5; i++)
     {
